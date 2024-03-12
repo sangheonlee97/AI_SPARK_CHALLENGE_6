eaba4254aac039500e66d39daf02f608e10d231a
sangheonlee97 <90169676+sangheonlee97@users.noreply.github.com>
2024-03-12 19:27:02 +0900

    segmentation_models의 Unet을 resnet152을 백본으로 넣고 사용

---
+++ src/unet.py
@@ -0,0 +1,259 @@
+import segmentation_models as sm
+import os
+import warnings
+warnings.filterwarnings("ignore")
+import glob
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import keras
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from tensorflow.python.keras import backend as K
+import sys
+import pandas as pd
+from tqdm import tqdm
+from keras.preprocessing.image import ImageDataGenerator
+import threading
+import random
+import rasterio
+import os
+import numpy as np
+import sys
+from sklearn.utils import shuffle as shuffle_lists
+from keras.models import *
+from keras.layers import *
+import numpy as np
+from keras import backend as K
+from sklearn.model_selection import train_test_split
+import joblib
+#######################################################################
+
+
+MAX_PIXEL_VALUE = 65535 # 이미지 정규화를 위한 픽셀 최대값
+
+class threadsafe_iter:
+    """
+    데이터 불러올떼, 호출 직렬화
+    """
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with self.lock:
+            return self.it.__next__()
+
+
+def threadsafe_generator(f):
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+
+    return g
+
+def get_img_arr(path):
+    img = rasterio.open(path).read().transpose((1, 2, 0))    
+    img = np.float32(img)/MAX_PIXEL_VALUE
+    
+    return img
+
+def get_img_762bands(path):
+    img = rasterio.open(path).read((7,6,2)).transpose((1, 2, 0))    
+    img = np.float32(img)/MAX_PIXEL_VALUE
+    
+    return img
+    
+def get_mask_arr(path):
+    img = rasterio.open(path).read().transpose((1, 2, 0))
+    seg = np.float32(img)
+    return seg
+
+
+
+@threadsafe_generator
+def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True, random_state=None, image_mode='10bands'):
+   
+    images = []
+    masks = []
+
+    fopen_image = get_img_arr
+    fopen_mask = get_mask_arr
+
+    if image_mode == '762':
+        fopen_image = get_img_762bands
+
+    i = 0 
+    # 데이터 shuffle
+    while True:
+        
+        if shuffle:
+            if random_state is None:
+                images_path, masks_path = shuffle_lists(images_path, masks_path)
+            else:
+                images_path, masks_path = shuffle_lists(images_path, masks_path, random_state= random_state + i)
+                i += 1 
+
+
+        for img_path, mask_path in zip(images_path, masks_path):
+            
+            img = fopen_image(img_path)
+            mask = fopen_mask(mask_path)
+            images.append(img)
+            masks.append(mask)
+
+            if len(images) >= batch_size:
+                yield (np.array(images), np.array(masks))
+                images = []
+                masks = []
+                
+
+
+    
+# 두 샘플 간의 유사성 metric
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
+    dice = K.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+    return dice
+
+# 픽셀 정확도를 계산 metric
+def pixel_accuracy (y_true, y_pred):
+    sum_n = np.sum(np.logical_and(y_pred, y_true))
+    sum_t = np.sum(y_true)
+ 
+    if (sum_t == 0):
+        pixel_accuracy = 0
+    else:
+        pixel_accuracy = sum_n / sum_t
+    return pixel_accuracy    
+
+# 사용할 데이터의 meta정보 가져오기
+
+train_meta = pd.read_csv('../resource/dataset/train_meta.csv')
+test_meta = pd.read_csv('../resource/dataset/test_meta.csv')
+
+
+# 저장 이름
+save_name = 'stack'
+
+N_FILTERS = 16 # 필터수 지정
+N_CHANNELS = 3 # channel 지정
+EPOCHS = 100 # 훈련 epoch 지정
+BATCH_SIZE = 16 # batch size 지정
+IMAGE_SIZE = (256, 256) # 이미지 크기 지정
+MODEL_NAME = 'unet' # 모델 이름
+RANDOM_STATE = 42 # seed 고정
+INITIAL_EPOCH = 0 # 초기 epoch
+
+# 데이터 위치
+IMAGES_PATH = '../resource/dataset/train_img/'
+MASKS_PATH = '../resource/dataset/train_mask/'
+
+# 가중치 저장 위치
+OUTPUT_DIR = '../resource/dataset/train_output/'
+WORKERS = -2
+
+# 조기종료
+EARLY_STOP_PATIENCE = 30
+
+# 중간 가중치 저장 이름
+CHECKPOINT_PERIOD = 5
+CHECKPOINT_MODEL_NAME = 'checkpoint-{}-{}-epoch_{{epoch:02d}}.hdf5'.format(MODEL_NAME, save_name)
+
+# 최종 가중치 저장 이름
+FINAL_WEIGHTS_OUTPUT = 'model_{}_{}_final_weights.h5'.format(MODEL_NAME, save_name)
+
+# 사용할 GPU 이름
+CUDA_DEVICE = 0
+
+
+# 저장 폴더 없으면 생성
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+
+# GPU 설정
+os.environ["CUDA_VISIBLE_DEVICES"] = str(CUDA_DEVICE)
+try:
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.compat.v1.Session(config=config)
+    K.set_session(sess)
+except:
+    pass
+
+try:
+    np.random.bit_generator = np.random._bit_generator
+except:
+    pass
+
+
+# train : val = 8 : 2 나누기
+x_tr, x_val = train_test_split(train_meta, test_size=0.2, random_state=RANDOM_STATE)
+print(len(x_tr), len(x_val))
+
+# train : val 지정 및 generator
+images_train = [os.path.join(IMAGES_PATH, image) for image in x_tr['train_img'] ]
+masks_train = [os.path.join(MASKS_PATH, mask) for mask in x_tr['train_mask'] ]
+
+images_validation = [os.path.join(IMAGES_PATH, image) for image in x_val['train_img'] ]
+masks_validation = [os.path.join(MASKS_PATH, mask) for mask in x_val['train_mask'] ]
+
+train_generator = generator_from_lists(images_train, masks_train, batch_size=BATCH_SIZE, random_state=RANDOM_STATE, image_mode="762")
+validation_generator = generator_from_lists(images_validation, masks_validation, batch_size=BATCH_SIZE, random_state=RANDOM_STATE, image_mode="762")
+
+#######################################################################
+BACKBONE = 'resnet152'
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+# load your data
+# x_train, y_train, x_val, y_val = load_data(...)
+
+# preprocess input
+# x_train = preprocess_input(x_train)
+# x_val = preprocess_input(x_val)
+
+# define model
+model = sm.Unet(BACKBONE, encoder_weights='imagenet', classes=1, activation='sigmoid', encoder_freeze=True)
+model.compile(
+    'Adam',
+    loss=sm.losses.bce_jaccard_loss,
+    metrics=[sm.metrics.iou_score],
+)
+
+model.summary()
+
+# fit model
+# if you use data generator use model.fit_generator(...) instead of model.fit(...)
+# more about `fit_generator` here: https://keras.io/models/sequential/#fit_generator
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=EARLY_STOP_PATIENCE, restore_best_weights=True)
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=len(images_train) // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=len(images_validation) // BATCH_SIZE,
+    epochs=EPOCHS,
+    workers=WORKERS,
+    callbacks=[es],
+)
+
+print('가중치 저장')
+model.save_weights('../resource/weights/resnet152.h5')
+print("저장된 가중치 명: resnet152.h5")
+
+print(model.evaluate_generator(validation_generator))
+y_pred_dict = {}
+
+for i in test_meta['test_img']:
+    img = get_img_762bands(f'../resource/dataset/test_img/{i}')
+    y_pred = model.predict(np.array([img]), batch_size=1)
+    
+    y_pred = np.where(y_pred[0, :, :, 0] > 0.25, 1, 0) # 임계값 처리
+    y_pred = y_pred.astype(np.uint8)
+    y_pred_dict[i] = y_pred
+
+joblib.dump(y_pred_dict, './y_pred.pkl')
